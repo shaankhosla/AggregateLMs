ebac0df656d9b19d8f25f802134589b3aa3df241
shaan <skhoslajr@gmail.com>
2022-04-17 22:01:50 -0400

    ignore models and minor

--- model_roberta/run_bagging.py
+++ model_roberta/run_bagging.py
@@ -11,13 +11,14 @@ import dataset
 import finetuning_utils
 import pandas as pd
 import torch
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import classification_report
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.model_selection import train_test_split
 from torch.nn.utils import prune
+from transformers import GPT2ForSequenceClassification
+from transformers import GPT2Tokenizer
 from transformers import RobertaForSequenceClassification
 from transformers import RobertaTokenizer
-from transformers import GPT2Tokenizer, GPT2ForSequenceClassification
 from transformers import Trainer
 from transformers import TrainingArguments
 from yaml import load
@@ -44,36 +45,30 @@ print("Data is in: ", args.data_dir)
 task_name = os.path.basename(args.data_dir)
 
 if task_name != "MultiRC":
-    train_df = pd.read_json(
-        f"{args.data_dir}/train.jsonl", lines=True, orient="records"
-    )
-    val_df, test_df = train_test_split(
-        pd.read_json(f"{args.data_dir}/val.jsonl", lines=True, orient="records"),
-        test_size=0.5,
-    )
+
+    test_df = pd.read_json(f"{args.data_dir}/val.jsonl", lines=True, orient="records")
+
 else:
-    train_df = data_utils.process_multirc_jsonl(f"{args.data_dir}/train.jsonl", " ")
-    val_df, test_df = train_test_split(
-        data_utils.process_multirc_jsonl(f"{args.data_dir}/val.jsonl", " "),
-        test_size=0.5,
-    )
+    test_df = data_utils.process_multirc_jsonl(f"{args.data_dir}/val.jsonl", " ")
+
+
+tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 
 
 def load_models(model_dir):
     print("Loading models from " + str(model_dir))
     models = []
-    tokenizers = []
     for directory in os.listdir(model_dir):
         print("dir" + directory)
-        if not os.path.isdir(model_dir+"/"+directory+"/"):
+        if not os.path.isdir(model_dir + "/" + directory + "/"):
             continue
-        directory = model_dir+"/"+directory+"/"
-        tokenizer = RobertaTokenizer.from_pretrained(directory)
-        model = RobertaForSequenceClassification.from_pretrained(directory,problem_type="multi_label_classification")
+        directory = model_dir + "/" + directory + "/"
+        model = RobertaForSequenceClassification.from_pretrained(
+            "roberta-base", problem_type="multi_label_classification"
+        )
         print("first model - " + str(type(model)))
         models.append(model)
-        tokenizers.append(tokenizer)
-    return models, tokenizers
+    return models
 
 
 def get_module_by_name(module, access_string):
@@ -105,24 +100,24 @@ def prune_models(models):
 
         for module, _ in module_tups:
             prune.remove(module, "weight")
-        #print(count_parameters(model), count_nonzero(model))
         new_models.append(model)
+        print(count_parameters(model), count_nonzero(model))
     print("models pruned!")
     return new_models
 
 
-
-def run_evaluation(models, tokenizers,task_name, test_df):
+def run_evaluation(models, task_name, test_df):
     y_preds = []
-    y_true = data_utils.extract_labels(test_df,task_name)[:5]
+    y_true = data_utils.extract_labels(test_df, task_name)[:50]
 
     for i in range(len(y_true)):
         votingDict = defaultdict(int)
         for model in models:
             if task_name == "MultiRC" or task_name == "BoolQ":
-                tokenizedinput = data_utils.encode_data(test_df[i:i+1],tokenizers[0],task_name)
-                print("Tokenized input \n")
-                print(tokenizedinput)
+                print(test_df[i : i + 1])
+                tokenizedinput = data_utils.encode_data(
+                    test_df[i : i + 1], tokenizer, task_name
+                )
                 with torch.no_grad():
                     logits = model(**tokenizedinput).logits
                     print(logits)
@@ -130,15 +125,16 @@ def run_evaluation(models, tokenizers,task_name, test_df):
                 votingDict[model.config.id2label[predicted_class_id]] += 1
             else:
                 continue
-        #print(votingDict)
         y_pred = max(votingDict, key=votingDict.get)
-        y_preds.append(int((y_pred.replace("LABEL_1","1").replace("LABEL_0","0"))))
-    target_names = ['0','1']
+        y_preds.append(int(y_pred.replace("LABEL_1", "1").replace("LABEL_0", "0")))
+    target_names = ["0", "1"]
     print(y_preds)
     return classification_report(y_true, y_preds, target_names=target_names)
-    #return precision_recall_fscore_support(y_true, y_preds, average="macro")
+    # return precision_recall_fscore_support(y_true, y_preds, average="macro")
+
 
 model_dir = "../models/"
-models,tokenizers = load_models(model_dir)
-#pruned_models = prune_models(models)
-print(run_evaluation(models, tokenizers,task_name, test_df))
+models = load_models(model_dir)
+print(len(models))
+# models = prune_models(models)
+run_evaluation(models, task_name, test_df)
