c10637a7e8a939e216976746557cb804e61f86ad
Pranab Islam <islampranab@gmail.com>
2022-05-08 23:15:28 -0400

    experiment params getter

--- get_experiment_params.py
+++ get_experiment_params.py
@@ -9,7 +9,7 @@ def get_experiment_configurations(config_number):
     config_number is type int
     '''
     experiment_table = pd.read_csv(experiment_table_csv_link).iloc[:,:7].dropna() ## Ensures only filled in data comes through
-    experiment_table[experiment_table['Configuration Num']==config_number]
+    experiment_table = experiment_table[experiment_table['Configuration Num']==config_number]
     model_paths = experiment_table['Path'].to_list()
     num_models_per_type = experiment_table['Number of Models'].astype(int).to_list()
     pruning_factors = experiment_table['Pruning Factor'].to_list()
